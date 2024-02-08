37dae7a76c0740de51cbf9f7607c055fbf5cb8b5
kysniw <kuba.winsyk.forgames@gmail.com>
2024-02-08 14:49:00 +0100

    Fixed contact form and added text responses

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -2,29 +2,67 @@ import { Element } from "react-scroll";
 import styles from "./Contact.module.css";
 import React, { useRef, useState } from "react";
 import { sendForm } from "@emailjs/browser";
+import { FaXmark } from "react-icons/fa6";
 
 const Contact = () => {
-  const formRef = useRef(null);
+  const formRef = useRef<HTMLFormElement>(null);
   const [fullName, setFullName] = useState("");
   const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [status, setStatus] = useState<{
+    message: string | null;
+    error: string | null;
+  }>({ message: null, error: null });
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
+
     // alert("Submited");
 
     if (formRef.current) {
+      setIsLoading(true);
       sendForm(
         `${process.env.REACT_APP_SERVICE_ID}`,
         `${process.env.REACT_APP_TEMPLATE_ID}`,
-        formRef.current as HTMLFormElement,
+        formRef.current,
         {
           publicKey: `${process.env.REACT_APP_PUBLIC_KEY}`,
         }
       )
-        .then(() => console.log("SUCCESS"))
-        .catch((error) => console.log(error));
+        .then(() => {
+          setFullName("");
+          setEmail("");
+          setPhoneNumber("");
+          setSubject("");
+          setMessage("");
+          setStatus({
+            message: "The message has been sent succesfully!",
+            error: null,
+          });
+          setIsLoading(false);
+        })
+        .catch((error) => {
+          setFullName("");
+          setEmail("");
+          setPhoneNumber("");
+          setSubject("");
+          setMessage("");
+          console.log(error);
+          setStatus({
+            message: null,
+            error: "Something went wrong! Try again later.",
+          });
+          setIsLoading(false);
+        });
       // console.log(res);
     }
+  };
+
+  const handleCloseStatus = () => {
+    setStatus({ message: null, error: null });
   };
 
   return (
@@ -39,6 +77,7 @@ const Contact = () => {
             placeholder="Full Name"
             value={fullName}
             onChange={(e) => setFullName(e.target.value)}
+            autoComplete="name"
             required
           />
           <input
@@ -48,6 +87,7 @@ const Contact = () => {
             placeholder="Email Address"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
+            autoComplete="email"
             required
           />
         </div>
@@ -59,6 +99,9 @@ const Contact = () => {
             maxLength={13}
             name="phone_number"
             placeholder="Mobile Number"
+            value={phoneNumber}
+            onChange={(e) => setPhoneNumber(e.target.value)}
+            autoComplete="tel"
           />
           <input
             className={styles.input}
@@ -66,6 +109,9 @@ const Contact = () => {
             name="subject"
             minLength={5}
             placeholder="Email Subject"
+            value={subject}
+            onChange={(e) => setSubject(e.target.value)}
+            autoComplete="off"
             required
           />
         </div>
@@ -76,9 +122,29 @@ const Contact = () => {
           rows={10}
           minLength={20}
           placeholder="Your Message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          autoComplete="off"
           required
         ></textarea>
-        <input className={styles.submit} type="submit" value="Send Message" />
+        <input
+          className={styles.submit}
+          type="submit"
+          value="Send Message"
+          disabled={isLoading}
+        />
+        {status.message && (
+          <div className={styles.success}>
+            <p>Message sent!</p>
+            <FaXmark className={styles.close} onClick={handleCloseStatus} />
+          </div>
+        )}
+        {status.error && (
+          <div className={styles.error}>
+            <p>Something went wrong!</p>
+            <FaXmark className={styles.close} onClick={handleCloseStatus} />
+          </div>
+        )}
       </form>
     </Element>
   );
